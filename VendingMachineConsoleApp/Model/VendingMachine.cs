0ceca8691608b03bae7e70c51b354c7fa64d9942
Shikyo <dennan_lol@hotmail.com>
2021-07-05 14:05:54 +0200

    Add project files.

---
+++ VendingMachineConsoleApp/Model/VendingMachine.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachineConsoleApp.Model
+{
+    class VendingMachine : IVending
+    {
+    }
+}
---
+++ VendingMachineConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VendingMachineConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static string Print(string whatToAsk)
+        {
+            Console.Write(whatToAsk);
+
+            return Console.ReadLine();  //Or readkey?
+        }
+
+
+    }
+}
